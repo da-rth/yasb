99ddc24e07b1a360e4618fc9543b8f0c5c4bdac7
denBot <den.xyz@pm.me>
2021-12-11 11:43:21 +0000

    feat+chore: Added backend utility functions for windows media contol and added Example widget / validation templates

---
+++ src/core/widgets/example.py
@@ -0,0 +1,54 @@
+from core.widgets.base import BaseWidget
+from core.validation.widgets.example import EXAMPLE_VALIDATION_SCHEMA
+from PyQt6.QtWidgets import QLabel
+
+
+class ExampleWidget(BaseWidget):
+    validation_schema = EXAMPLE_VALIDATION_SCHEMA
+
+    def __init__(
+            self,
+            label: str,
+            label_alt: str,
+            update_interval: int,
+            callbacks: dict[str, str],
+    ):
+        super().__init__(update_interval, class_name="template-widget")
+        self._show_alt_label = False
+        self._label_content = label
+        self._label_alt_content = label_alt
+
+        self._label = QLabel()
+        self._label_alt = QLabel()
+        self._label.setProperty("class", "label")
+        self._label_alt.setProperty("class", "label alt")
+        self.widget_layout.addWidget(self._label)
+        self.widget_layout.addWidget(self._label_alt)
+        self.register_callback("toggle_label", self._toggle_label)
+
+        self.callback_left = callbacks['on_left']
+        self.callback_right = callbacks['on_right']
+        self.callback_middle = callbacks['on_middle']
+        self.callback_timer = "update_label"
+
+        self._label.show()
+        self._label_alt.hide()
+
+        self.start_timer()
+
+    def _toggle_label(self):
+        self._show_alt_label = not self._show_alt_label
+
+        if self._show_alt_label:
+            self._label.hide()
+            self._label_alt.show()
+        else:
+            self._label.show()
+            self._label_alt.hide()
+
+        self._update_label_content()
+
+    def _update_label(self):
+        # Update the label at each timer interval
+        self._label.setText(self._label_content)
+        self._label_alt.setText(self._label_alt_content)
