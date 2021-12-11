f4564e78ad38d566b9d23340da3d3835018b8471
denBot <den.xyz@pm.me>
2021-12-11 11:57:39 +0000

    chore: Refactored ClockWidget to use example widget structure

--- src/core/widgets/yasb/clock.py
+++ src/core/widgets/yasb/clock.py
@@ -1,9 +1,9 @@
 import re
 import pytz
-from datetime import datetime
 from core.widgets.base import BaseWidget
 from core.validation.widgets.yasb.clock import VALIDATION_SCHEMA
 from PyQt6.QtWidgets import QLabel
+from datetime import datetime
 from tzlocal import get_localzone_name
 from itertools import cycle
 
@@ -20,54 +20,69 @@ class ClockWidget(BaseWidget):
             callbacks: dict[str, str],
     ):
         super().__init__(update_interval, class_name="clock-widget")
-        self._show_alt = False
-        self._label = label
-        self._label_alt = label_alt
         self._active_tz = None
-        self._active_label = label
         self._timezones = cycle(timezones if timezones else [get_localzone_name()])
-        self._clock_text = QLabel()
-        self._clock_text.setProperty("class", "clock-label")
         self._active_datetime_format_str = ''
         self._active_datetime_format = None
-        self.widget_layout.addWidget(self._clock_text)
 
-        self.register_callback("toggle_label", self.toggle_clock_text)
-        self.register_callback("update_clock_text", self.update_clock_text)
-        self.register_callback("next_timezone", self.next_timezone)
+        self._label_content = label
+        self._label_alt_content = label_alt
+
+        self._label = QLabel()
+        self._label_alt = QLabel()
+        self._label.setProperty("class", "label")
+        self._label_alt.setProperty("class", "label alt")
+        self.widget_layout.addWidget(self._label)
+        self.widget_layout.addWidget(self._label_alt)
+        self.register_callback("toggle_label", self._toggle_label)
+
+        self.register_callback("toggle_label", self._toggle_label)
+        self.register_callback("update_label", self._update_label)
+        self.register_callback("next_timezone", self._next_timezone)
 
         self.callback_left = callbacks['on_left']
         self.callback_right = callbacks['on_right']
         self.callback_middle = callbacks['on_middle']
-        self.callback_timer = "update_clock_text"
+        self.callback_timer = "update_label"
 
-        self.next_timezone()
-        self.update_timestamp_format()
+        self._label.show()
+        self._label_alt.hide()
+        self._show_alt_label = False
+
+        self._next_timezone()
+        self._update_label()
         self.start_timer()
 
-    def toggle_clock_text(self):
-        self._show_alt = not self._show_alt
-        self._active_label = self._label_alt if self._show_alt else self._label
-        self.update_timestamp_format()
-        self.update_clock_text()
+    def _toggle_label(self):
+        self._show_alt_label = not self._show_alt_label
 
-    def update_timestamp_format(self):
-        datetime_format_search = re.search('\{(.*)}', self._active_label)
-        self._active_datetime_format_str = datetime_format_search.group()
-        self._active_datetime_format = datetime_format_search.group(1)
+        if self._show_alt_label:
+            self._label.hide()
+            self._label_alt.show()
+        else:
+            self._label.show()
+            self._label_alt.hide()
 
-    def next_timezone(self):
+        self._update_label()
+
+    def _update_label(self):
+        active_label = self._label_alt if self._show_alt_label else self._label
+        active_label_content = self._label_alt_content if self._show_alt_label else self._label_content
+
+        try:
+            datetime_format_search = re.search('\{(.*)}', active_label_content)
+            datetime_format_str = datetime_format_search.group()
+            datetime_format = datetime_format_search.group(1)
+            datetime_now = pytz.utc.localize(datetime.utcnow()).astimezone(pytz.timezone(self._active_tz))
+            format_label_content = active_label_content.replace(
+                datetime_format_str,
+                datetime_now.strftime(datetime_format)
+            )
+            active_label.setText(format_label_content)
+        except Exception:
+            active_label.setText(active_label_content)
+
+    def _next_timezone(self):
         self._active_tz = next(self._timezones)
         self.setToolTip(self._active_tz)
-        self.update_clock_text()
-
-    def update_clock_text(self):
-        try:
-            dt_now = pytz.utc.localize(datetime.utcnow()).astimezone(pytz.timezone(self._active_tz))
-            clock_text = self._active_label.replace(
-                self._active_datetime_format_str,
-                dt_now.strftime(self._active_datetime_format)
-            )
-            self._clock_text.setText(clock_text)
-        except Exception:
-            self._clock_text.setText(self._active_label)
+        self._update_label()
