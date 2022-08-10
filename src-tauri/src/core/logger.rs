3c1c8579b6655fa766a6658fe143b94f92d5f474
es183923 <72744903+es183923@users.noreply.github.com>
2022-08-10 18:01:32 -0400

    fix: Create config dir if it doesn't exist

--- src-tauri/src/core/logger.rs
+++ src-tauri/src/core/logger.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, path::PathBuf};
+use std::{fs::{File, create_dir_all}, path::PathBuf};
 use home::home_dir;
 use simplelog::{
   CombinedLogger,
@@ -11,31 +11,35 @@ use simplelog::{
 use crate::core::constants::{CONFIG_DIR_NAME, APP_LOG_FILENAME};
 
 pub fn init_logger(verbose: bool) -> PathBuf {
-  let home_path = home_dir();
-  
-  if !home_path.is_some() {
+  if let Some(home_path) = home_dir() {
+
+    let mut log_path = home_path;
+    log_path.push(CONFIG_DIR_NAME);
+    log_path.push(APP_LOG_FILENAME);
+
+    create_dir_all(log_path.parent().unwrap()).unwrap_or_else(|_| {
+      eprintln!("Error creating logger directory");
+    });
+
+    CombinedLogger::init(
+      vec![
+        TermLogger::new(
+          LevelFilter::Info,
+          Config::default(),
+          TerminalMode::Mixed,
+          if verbose { ColorChoice::Never } else { ColorChoice::Auto }
+        ),
+        WriteLogger::new(
+          LevelFilter::Info,
+          Config::default(),
+          File::create(log_path.clone()).unwrap()
+        )
+      ]
+    ).expect("Failed to initialise logger");
+
+    log_path
+  } else {
     eprintln!("No home directory could be fine. Exiting...");
     std::process::exit(1);
   }
-
-  let mut log_path = home_dir().unwrap();
-  log_path.push(format!("{}/{}", CONFIG_DIR_NAME, APP_LOG_FILENAME));
-
-  CombinedLogger::init(
-    vec![
-      TermLogger::new(
-        LevelFilter::Info,
-        Config::default(),
-        TerminalMode::Mixed,
-        if verbose { ColorChoice::Never } else { ColorChoice::Auto }
-      ),
-      WriteLogger::new(
-        LevelFilter::Info,
-        Config::default(),
-        File::create(log_path.clone()).unwrap()
-      )
-    ]
-  ).expect("Failed to initialise logger");
-
-  log_path
 }
