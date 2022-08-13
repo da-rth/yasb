d17ded1045a74d9592a9e187fedadfa8e9e4bef8
es183923 <72744903+es183923@users.noreply.github.com>
2022-08-13 01:35:48 +0000

    revert format commit

--- src-tauri/src/core/constants.rs
+++ src-tauri/src/core/constants.rs
@@ -4,7 +4,7 @@ pub const FRONTEND_SETUP: &str = "setup.html";
 pub const FRONTEND_INDEX: &str = "index.html";
 
 pub const APP_LOG_FILENAME: &str = "yasb.log";
-pub const CONFIG_DIR_NAME: &str = "yasb-rs";
+pub const CONFIG_DIR_NAME: &str = "yasb";
 pub const CONFIG_FILENAME: &str = "config.yaml";
 pub const STYLES_FILENAME: &str = "styles.scss";
 
