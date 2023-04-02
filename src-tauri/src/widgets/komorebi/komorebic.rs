f82e877f74155753c083de16e89f93148497d315
denBot <den.xyz@pm.me>
2023-04-02 17:06:10 +0100

    fix: Prevent komorebi subscribe/float-rule commands from creating window

--- src-tauri/src/widgets/komorebi/komorebic.rs
+++ src-tauri/src/widgets/komorebi/komorebic.rs
@@ -1,28 +1,29 @@
-use std::process::{Command, ExitStatus, Stdio};
+use std::os::windows::process::CommandExt;
+use std::process::ExitStatus;
+use std::process::{Command, Stdio};
 
 use crate::core::constants::APP_NAME;
 
 const KOMOREBI_CLI_EXE: &str = "komorebic.exe";
+const CREATE_NO_WINDOW: u32 = 0x08000000;
 
 pub fn subscribe(pipe_name: &str) -> ExitStatus {
-    let mut child = Command::new(KOMOREBI_CLI_EXE)
-        .arg("subscribe")
-        .arg(pipe_name)
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .spawn()
-        .unwrap();
-    child.wait().unwrap()
+    let mut cmd = Command::new(KOMOREBI_CLI_EXE);
+    cmd.arg("subscribe");
+    cmd.arg(pipe_name);
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+    cmd.creation_flags(CREATE_NO_WINDOW);
+    cmd.spawn().unwrap().wait().unwrap()
 }
 
 pub fn unmanage_app_exe() -> ExitStatus {
-    let mut child = Command::new(KOMOREBI_CLI_EXE)
-        .arg("float-rule")
-        .arg("exe")
-        .arg(format!("{}.exe", APP_NAME))
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .spawn()
-        .unwrap();
-    child.wait().unwrap()
+    let mut cmd = Command::new(KOMOREBI_CLI_EXE);
+    cmd.arg("float-rule");
+    cmd.arg("exe");
+    cmd.arg(format!("{}.exe", APP_NAME));
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+    cmd.creation_flags(CREATE_NO_WINDOW);
+    cmd.spawn().unwrap().wait().unwrap()
 }
