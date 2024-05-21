03f5bbd07376377c2066816131eedaae9c546532
Rob Schreiber <indred0@gmail.com>
2024-05-21 16:56:58 -0600

    fix: Fix Monitor Specific Active Window Widget

--- src/core/widgets/yasb/active_window.py
+++ src/core/widgets/yasb/active_window.py
@@ -95,9 +95,9 @@ class ActiveWindowWidget(BaseWidget):
                 win_info['class_name'] in IGNORED_YASB_CLASSES):
             return
 
-        monitor_name = win_info['monitor_info'].get('device', None)
+        monitor_xpos = win_info['monitor_info'].get('rect', None).get('x', None)
 
-        if self._monitor_exclusive and self.screen().name() != monitor_name:
+        if self._monitor_exclusive and self.screen().geometry().x() != monitor_xpos:
             self._window_title_text.hide()
         else:
             self._update_window_title(hwnd, win_info, event)
